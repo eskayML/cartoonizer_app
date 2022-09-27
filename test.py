ba788c34c5f90fc635dc6af038e2e90bba5a7490
Samuel <kalusam619@gmail.com>
2022-09-27 12:24:51 +0100

    final: changes for tuesday

--- test.py
+++ test.py
@@ -17,20 +17,21 @@ if img is None:
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-img_blur = cv2.medianBlur(img_grey, 3)
+img_blur = cv2.medianBlur(img_grey, 5)
 
 edges = cv2.adaptiveThreshold(
-    img_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2 . THRESH_BINARY, 3, 3)
+    img_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2 . THRESH_BINARY, 9,9)
 
 
-img_bb = cv2.bilateralFilter(img_blur, 15, 75, 75)
+img_bb = cv2.bilateralFilter(img_blur, 9,250,250)
 
-kernel = np.ones((1, 1), np . uint8)
-img_erode = cv2.erode(img_bb, kernel, iterations=3)
-img_dilate = cv2.dilate(img_erode, kernel, iterations=3)
+# kernel = np.ones((1, 1), np . uint8)
+# img_erode = cv2.erode(img_bb, kernel, iterations=3)
+# img_dilate = cv2.dilate(img_erode, kernel, iterations=3)
 
-img_style = cv2.stylization(img, sigma_s=150, sigma_r=0.25)
+# img_style = cv2.stylization(img, sigma_s=150, sigma_r=0.25)
 
-cv2.imshow('image', img_style)
+cartoon = cv2.bitwise_and(img_bb,img_bb, mask=edges)
+cv2.imshow('image', cartoon)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
